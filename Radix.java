2127df6e67aaaf585daf85f1506c24f2a7675a06
Andrew <ajuang20@stuy.edu>
2021-01-12 12:57:06 -0500

    Added length method

--- Radix.java
+++ Radix.java
@@ -1,10 +1,11 @@
 public class Radix{
     public static int nth(int n, int col){
-        return Math.abs(n / Math.pow(10,col)) % 10; 
+        return Math.abs(n / Math.pow(10,col)) % 10;
     }
 
     public static int length(int n){
-
+        double a = 1.0 * Math.abs(n);
+        return (int)(Math.log10(a)+1);
     }
 
     public static void merge(MyLinkedList original, MyLinkedList[]buckets){
